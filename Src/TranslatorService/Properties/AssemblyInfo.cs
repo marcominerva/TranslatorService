bfc31df1a14dcd6d8a12c70a81fa6ba5539b743d
Marco Minerva <marco.minerva@gmail.com>
2017-01-04 17:17:22 +0100

    Incremented version number

--- Src/TranslatorService/Properties/AssemblyInfo.cs
+++ Src/TranslatorService/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TranslatorService")]
-[assembly: AssemblyCopyright("Copyright © Marco Minerva 2016")]
+[assembly: AssemblyCopyright("Copyright © Marco Minerva 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
